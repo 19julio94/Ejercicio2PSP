930969df47832a312d4a335e289c5d68982cf855
jpatriciodasilva <jpatriciodasilva@nautilus10.danielcastelao.org>
2016-10-10 10:25:09 +0200

    FINAL

---
+++ src/ejercicio2psp/Ejercicio2PSP.java
@@ -0,0 +1,14 @@
+
+package ejercicio2psp;
+
+
+public class Ejercicio2PSP {
+
+    
+    public static void main(String[] args) {
+       
+        new miThread("Fio1").start();
+        new miThread("Fio2").start();
+    }
+    
+}
